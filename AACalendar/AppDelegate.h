bfce411ade41d2b8fa86b4454be9643ada089497
Akshay Awtade <akshayawtade.123@gmail.com>
2017-05-24 21:15:20 +0530

    Initial Commit

---
+++ AACalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AACalendar
+//
+//  Created by AKSHAY AWTADE on 24/05/17.
+//  Copyright © 2017 AKSHAY AWTADE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AACalendar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AACalendar
+//
+//  Created by AKSHAY AWTADE on 24/05/17.
+//  Copyright © 2017 AKSHAY AWTADE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
