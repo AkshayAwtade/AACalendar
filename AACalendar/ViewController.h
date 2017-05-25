8b78efdf7466747c1ba0bdafba5af3eb2aa710b5
Akshay Awtade <akshayawtade.123@gmail.com>
2017-05-25 20:58:02 +0530

    Added layout and created outlets for layout

--- AACalendar/ViewController.h
+++ AACalendar/ViewController.h
@@ -9,7 +9,47 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
+@property (weak, nonatomic) IBOutlet UILabel *labelMonth;
+@property (weak, nonatomic) IBOutlet UILabel *labelYear;
+@property (weak, nonatomic) IBOutlet UIButton *but1;
+@property (weak, nonatomic) IBOutlet UIButton *but2;
+@property (weak, nonatomic) IBOutlet UIButton *but3;
+@property (weak, nonatomic) IBOutlet UIButton *but4;
+@property (weak, nonatomic) IBOutlet UIButton *but5;
+@property (weak, nonatomic) IBOutlet UIButton *but6;
+@property (weak, nonatomic) IBOutlet UIButton *but7;
+@property (weak, nonatomic) IBOutlet UIButton *but8;
+@property (weak, nonatomic) IBOutlet UIButton *but9;
+@property (weak, nonatomic) IBOutlet UIButton *but10;
+@property (weak, nonatomic) IBOutlet UIButton *but11;
+@property (weak, nonatomic) IBOutlet UIButton *but12;
+@property (weak, nonatomic) IBOutlet UIButton *but13;
+@property (weak, nonatomic) IBOutlet UIButton *but14;
+@property (weak, nonatomic) IBOutlet UIButton *but15;
+@property (weak, nonatomic) IBOutlet UIButton *but16;
+@property (weak, nonatomic) IBOutlet UIButton *but17;
+@property (weak, nonatomic) IBOutlet UIButton *but18;
+@property (weak, nonatomic) IBOutlet UIButton *but19;
+@property (weak, nonatomic) IBOutlet UIButton *but20;
+@property (weak, nonatomic) IBOutlet UIButton *but21;
+@property (weak, nonatomic) IBOutlet UIButton *but22;
+@property (weak, nonatomic) IBOutlet UIButton *but23;
+@property (weak, nonatomic) IBOutlet UIButton *but24;
+@property (weak, nonatomic) IBOutlet UIButton *but25;
+@property (weak, nonatomic) IBOutlet UIButton *but26;
+@property (weak, nonatomic) IBOutlet UIButton *but27;
+@property (weak, nonatomic) IBOutlet UIButton *but28;
+@property (weak, nonatomic) IBOutlet UIButton *but29;
+@property (weak, nonatomic) IBOutlet UIButton *but30;
+@property (weak, nonatomic) IBOutlet UIButton *but31;
+@property (weak, nonatomic) IBOutlet UIButton *but32;
+@property (weak, nonatomic) IBOutlet UIButton *but33;
+@property (weak, nonatomic) IBOutlet UIButton *but34;
+@property (weak, nonatomic) IBOutlet UIButton *but35;
+- (IBAction)butTapped:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *buttonPreviousYear;
+@property (weak, nonatomic) IBOutlet UIButton *buttonNextYear;
+@property (weak, nonatomic) IBOutlet UIButton *buttonNextMonth;
+@property (weak, nonatomic) IBOutlet UIButton *buttonPreviousMonth;
 @end
 
--- AACalendar/ViewController.m
+++ AACalendar/ViewController.m
@@ -8,14 +8,35 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSMutableArray *arrayYears;
+    NSMutableArray *monthArray;
+}
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    enum monthName {January=1, February=2,March=3,April=4,May=5,June=6,July=7,August=8,September=9,October=10,November=11,December = 12};
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    arrayYears=[[NSMutableArray alloc]init];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy"];
+    int year = [[dateFormatter stringFromDate:[NSDate date]] intValue];
+    NSLog(@"%d",year);
+    NSDateFormatter *dateFormatterMonth = [[NSDateFormatter alloc] init];
+    [dateFormatterMonth setDateFormat:@"mm"];
+    int month = [[dateFormatterMonth stringFromDate:[NSDate date]] intValue];
+    NSLog(@"%d",month);
+    [self monthCalculation:month];
+
+//    for(int inityear=1970;inityear<=2100;inityear++){
+//        NSString *stringyear=[NSString stringWithFormat:@"%d",inityear];
+//        [arrayYears addObject:stringyear];
+//    }
+//    NSLog(@"%@",arrayYears);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +46,110 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)monthCalculation:(int)month{
+//    NSDateFormatter *dateFormatterMonth = [[NSDateFormatter alloc] init];
+//    [dateFormatterMonth setDateFormat:@"mm"];
+//    int month = [[dateFormatterMonth stringFromDate:[NSDate date]] intValue];
+//    NSLog(@"%d",month);
+    NSString *monthName;
+    switch (month) {
+        case 1:
+            monthName=@"January";
+            break;
+        case 2:
+            monthName=@"February";
+            break;
+        case 3:
+            monthName=@"March";
+            break;
+        case 4:
+            monthName=@"April";
+            break;
+        case 5:
+            monthName=@"May";
+            break;
+        case 6:
+            monthName=@"June";
+            break;
+        case 7:
+            monthName=@"July";
+            break;
+        case 8:
+            monthName=@"August";
+            break;
+        case 9:
+            monthName=@"September";
+            break;
+        case 10:
+            monthName=@"October";
+            break;
+        case 11:
+            monthName=@"November";
+            break;
+        case 12:
+            monthName=@"December";
+            break;
+            
+        default:
+            break;
+    }
+    self.labelMonth.text=[NSString stringWithFormat:@"%@",monthName];
+    
+    
+}
+-(void)monthCalculation{
+        NSDateFormatter *dateFormatterMonth = [[NSDateFormatter alloc] init];
+        [dateFormatterMonth setDateFormat:@"mm"];
+        int month = [[dateFormatterMonth stringFromDate:[NSDate date]] intValue];
+        NSLog(@"%d",month);
+    NSString *monthName;
+    switch (month) {
+        case 1:
+            monthName=@"January";
+            break;
+        case 2:
+            monthName=@"February";
+            break;
+        case 3:
+            monthName=@"March";
+            break;
+        case 4:
+            monthName=@"April";
+            break;
+        case 5:
+            monthName=@"May";
+            break;
+        case 6:
+            monthName=@"June";
+            break;
+        case 7:
+            monthName=@"July";
+            break;
+        case 8:
+            monthName=@"August";
+            break;
+        case 9:
+            monthName=@"September";
+            break;
+        case 10:
+            monthName=@"October";
+            break;
+        case 11:
+            monthName=@"November";
+            break;
+        case 12:
+            monthName=@"December";
+            break;
+            
+        default:
+            break;
+    }
+    self.labelMonth.text=[NSString stringWithFormat:@"%@",monthName];
+    
+    
+}
 
+- (IBAction)butTapped:(id)sender {
+    [self monthCalculation];
+}
 @end
